674739e9ed6870543ac056373c58b659d3664cf7
gregodiaz <diazgregoriocruz@gmail.com>
2022-12-25 19:11:01 -0300

    Add form with new task name.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
-function App() {
+import { useState } from "react"
+
+type FormElement = React.FormEvent<HTMLFormElement>
+
+function App(): JSX.Element {
+
+  const [newTask, setNewTask] = useState<string>('')
+
+  const handleSubmit = (e: FormElement): void => {
+    e.preventDefault()
+    console.log(newTask)
+    setNewTask('')
+  }
+
   return (
-    <div className="App">
-      Todo
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={e => setNewTask(e.target.value)}
+          value={newTask}
+          autoFocus
+        />
+        <button>
+          Save
+        </button>
+      </form>
     </div>
   )
 }
