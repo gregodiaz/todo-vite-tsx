b467c92fa1e2fe5868074dddb69b3ca5008a39e0
gregodiaz <diazgregoriocruz@gmail.com>
2022-12-25 19:28:19 -0300

    Add task list and show them.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import { useState } from "react"
 
 type FormElement = React.FormEvent<HTMLFormElement>
+interface ITask {
+  name: string;
+  done: boolean;
+}
 
 function App(): JSX.Element {
 
   const [newTask, setNewTask] = useState<string>('')
+  const [tasks, setTasks] = useState<ITask[]>([])
 
   const handleSubmit = (e: FormElement): void => {
     e.preventDefault()
-    console.log(newTask)
+    addTask(newTask)
     setNewTask('')
+  }
+
+  const addTask = (name: string): void => {
+    const updatedTask = [...tasks, { name, done: false }]
+    setTasks(updatedTask)
   }
 
   return (
@@ -25,6 +35,13 @@ function App(): JSX.Element {
           Save
         </button>
       </form>
+      {
+        tasks.map((task: ITask, i: number) => (
+          <div key={i}>
+            {task.name}
+          </div>
+        ))
+      }
     </div>
   )
 }
